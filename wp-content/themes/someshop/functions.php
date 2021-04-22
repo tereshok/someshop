b9558de571ad2dc2066492d743a9dd7c63fdc157
Alexandr <kwon_wtf@ukr.net>
2021-04-22 16:18:00 +0300

    ADDED | new pages

---
+++ wp-content/themes/someshop/functions.php
@@ -0,0 +1,6 @@
+<?php
+define('THEME_DIR', get_template_directory());
+define('THEME_DIR_URI', get_template_directory_uri());
+
+require_once THEME_DIR .'/inc/helpers/file.php';
+require_once THEME_DIR .'/inc/wp/enqueue_scripts.php';
