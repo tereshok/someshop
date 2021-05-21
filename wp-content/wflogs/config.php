de5adf5964dfdfe34e76aa51142fde4359f7c374
tereshok <43421381+tereshok@users.noreply.github.com>
2021-05-21 12:11:38 +0300

    UPDATE | conf files

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1622183804;s:7:"authKey";s:64:"q3,9=<a)/?):{.jD&`b0})_,,^Nt@E#)pku`oltaog[)_RU*`X)G, tD$*FSm7J7";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:17;}
